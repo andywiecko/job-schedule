6e65ac129a9f862c3fcf97581ec0a82b1e0f8cf3
andywiecko <awiecko@us.edu.pl>
2019-02-07 21:25:32 +0100

    draft version

---
+++ config.py
@@ -0,0 +1,1 @@
+max_running_jobs = 1
--- schedule.py
+++ schedule.py
@@ -1,7 +1,12 @@
 #!/bin/python
 # ======================================================================
 __author__ = "andywiecko"
-__version__ = "1.0"
+__version__ = "1.2"
+# TODO
+# - make some clean up
+# - ... some defs
+# - ... queue the jobs
+# - hide remaining jobs in one function
 # ======================================================================
 import os
 import subprocess
@@ -45,6 +50,7 @@ queue_jobs = open(filename,'r').read().split('\n')[:-1]
 all_jobs = len(queue_jobs)
 # jobs done counter
 done_jobs = 0
+import config
 # ======================================================================
 # ===================== INITIALIZATION WORKERS =========================
 # ======================================================================
@@ -53,24 +59,56 @@ for i in range(all_jobs if max_running_jobs > all_jobs else max_running_jobs):
 # ======================================================================
 # ===================== POPING JOBS FROM QUEUE =========================
 # ======================================================================
+from datetime import datetime
+
+tmp_max_running_jobs = max_running_jobs
+TIME = time.time()
+doneOnTime = 0
 while (queue_jobs != []):
-    time.sleep(sync_time)
-    for i in range(max_running_jobs):
-        proc = running_jobs[i]
-        
-        if proc.poll() == 0:
-            if queue_jobs == []: break
-            running_jobs[i] = subprocess.Popen(queue_jobs.pop(), shell = True)
-            done_jobs += 1
-            print 'jobs done: ',done_jobs,'/', all_jobs,' (workers: ',len(running_jobs),')'
+     time.sleep(sync_time)
+
+     try:
+          reload(config)
+          error = ""
+     except:
+          error = "Syntax ERROR: there is an error in config.py file\n" 
+     tmp_max_running_jobs = config.max_running_jobs
+     if tmp_max_running_jobs > max_running_jobs:
+          TIME = time.time()
+          doneOnTime = 0
+          diff = tmp_max_running_jobs - max_running_jobs
+          for j in range(diff):
+               running_jobs.append(subprocess.Popen(queue_jobs.pop(), shell = True))
+          max_running_jobs = tmp_max_running_jobs
+
+     for i in range(max_running_jobs):
+          proc = running_jobs[i]
+          if proc.poll() != None:
+               if queue_jobs == []: break
+
+               if tmp_max_running_jobs < max_running_jobs:
+                    TIME = time.time()
+                    doneOnTime = 0
+                    running_jobs.remove(proc)
+                    max_running_jobs -= 1
+                    break
+                    
+               else:
+                    running_jobs[i] = subprocess.Popen(queue_jobs.pop(), shell = True)
+                    done_jobs += 1
+                    doneOnTime += 1
+                    print 'jobs done: ',done_jobs,'/', all_jobs,' (workers: ',len(running_jobs),')'
+                    deltaTIME = (time.time() - TIME) / doneOnTime
+                    deltaTIME = deltaTIME*(all_jobs-done_jobs)
+                    print datetime.fromtimestamp(time.time()+deltaTIME).strftime("%A, %B %d, %Y %I:%M:%S")
+
 # ======================================================================
 # ====================== REMAINING LAST JOBS ===========================
 # ======================================================================
 while (running_jobs != []):
     time.sleep(sync_time)
     for proc in running_jobs:
-
-        if proc.poll() == 0:
+        if proc.poll() != None:
             running_jobs.remove(proc)
             done_jobs += 1
             print 'jobs done: ',done_jobs,'/', all_jobs,' (workers: ',len(running_jobs),')'
